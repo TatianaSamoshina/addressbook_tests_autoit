eb32457c851816f614b1e5864ccbae4784dbbd94
TatianaSamoshina <tatjana.kolesnickowa@yandex.ru>
2024-06-01 11:57:58 +0300

    18

---
+++ addressbook_tests_autoit/test/GroupCreationTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTest : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldgroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData() { Name = "test" };
+
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newgroups = app.Groups.GetGroupList();
+            oldgroups.Add(newGroup);
+            oldgroups.Sort();
+            newgroups.Sort();
+            Assert.AreEqual(oldgroups, newgroups);
+        }
+    }
+}
---
+++ addressbook_tests_autoit/test/GroupDeletionTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupDeletionTest : TestBase
+    {
+        [Test]
+        public void TestGroupDeletion()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            if (oldGroups.Count == 0) // Если нет групп, создаем новую
+            {
+                GroupData newGroup = new GroupData() { Name = "test" };
+                app.Groups.Add(newGroup);
+                oldGroups = app.Groups.GetGroupList();
+            }
+
+            app.Groups.Remove(0); 
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(0);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
